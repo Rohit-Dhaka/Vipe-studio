393e5aeab825d956f3d9a2b2b607f2caca270fcf
Rohit-Dhaka <rohitdhaka2110@gamil.com>
2024-01-19 11:40:43 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily:{
+        'Gilroy-font': ('Gilroy-black'),
+        'Konexy-font' : ('Konexy-font'),
+        'jost': ('Jost', 'sans-serif'),
+        'inter': ('Inter' , 'sans-serif')
+      },
+      boxShadow:{
+        'gradient-shadow' : '0px 20px 50px 0px rgba(39, 0, 124, 0.50)' ,
+        'card-shadow' : ' 0px 20px 60.571px 0px #E5E9F6',
+        'min-card': '12.114px 48.457px 60.571px 0px rgba(229, 233, 246, 0.40)'
+      }
+    },
+  },
+  plugins: [],
+}
+
